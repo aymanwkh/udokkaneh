451971a945d9d7336d9a8ad207bd8e92a5095ef3
ayman.wkh <ayman.wkh@gmail.com>
2021-03-07 06:41:18 +0200

    switch to ionic

---
+++ src/iapp.tsx
@@ -0,0 +1,43 @@
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router-dom'
+
+import Store from './data/store'
+import Home from './pages/ihome'
+import Basket from './pages/ibasket'
+import Search from './pages/isearch'
+
+const app = () => {
+  return (
+    <Store>
+      <IonApp>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route exact path="/" component={Home} />
+            <Route path="/home" component={Home} />
+            <Route path="/search" component={Search} />
+            <Route path="/basket" component={Basket} />
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </IonApp>
+    </Store>
+  )
+}
+
+export default app
---
+++ src/pages/ibasket.tsx
@@ -0,0 +1,11 @@
+import { IonPage } from '@ionic/react';
+
+const Basket = () => {
+  return (
+    <IonPage>
+      Basket
+    </IonPage>
+  )
+}
+
+export default Basket
